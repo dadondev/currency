fe5107a7236b335cff4db7ca0a423d1c58fbbe26
dadondev <dadondev03@gmail.com>
2024-04-13 21:20:31 +0500

    hello

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -13,10 +13,9 @@ const HeaderContainer = styled.header`
   padding: 20px 15px 25px;
   width: 100%;
   z-index: 100;
-  background: linear-gradient(92.67deg, #bfc3fc 0.11%, #a2c3fc 100%);
+  background: ${(prev) => prev.theme.colors.primaryGradient};
   box-shadow: 0px 4px 38px rgba(0, 0, 0, 0.05);
   border-radius: 0px 0px 40px 40px;
-
   @media (width>=768px) {
     padding-inline: 30px;
   }
